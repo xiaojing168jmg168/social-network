38ef82ef323c5ad66526726b70282aa90835c56c
Xiaojing Deng <8redfishcreative@gmail.com>
2022-12-19 10:15:41 -0500

    add/delete friend from user

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,40 @@ module.exports = {
         : res.json(user)
      )
      .catch((err)=> res.status(500).json(err));
-    }
+    },
+    // Add a friend to a User
+    addFriend(req, res){
+        console.log('You are adding a friend');
+        console.log(req.body);
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$addToSet: {friends: req.body}},
+            {runValidators: true, new: true}
+        )
+            .then((user) =>
+                !user
+                ? res
+                    .status(404)
+                    .json({message:'No user found with this id!'})
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Remove friend from a user
+    removeFriend(req, res){
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$pull: {friends: {friendId: req.params.friendId}}},
+            {runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user
+            ? res
+                .status(404)
+                .json({message:'No user found with that id!'})
+            : res.json(user)
+       )
+       .catch((err) => res.status(500).json(err));
+    },
     
-}
+};
