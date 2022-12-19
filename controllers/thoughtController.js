0db18047a1f5164c7c48377a610bc032d5348989
Xiaojing Deng <8redfishcreative@gmail.com>
2022-12-19 12:00:51 -0500

    create/delete reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -71,5 +71,32 @@ module.exports = {
         )
         .catch((err) => res.status(500).json(err));
     },
-
-}
+    // Create reaction
+    createReaction(req, res){
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$addToSet: {reactions: req.body}},
+            {runValidators: true, new: true}
+        )
+        .then((thought) =>
+            !thought
+              ? res.status(404).json({message: 'No thought found with this id!'})
+              : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // Delete reaction
+    deleteReaction(req, res){
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$pull: {reaction: {reactionId: req.params.reactionId}}},
+            {runValidators: true, new: true}
+        )
+        .then((thought) =>
+          !thought
+              ? res.status(404).json({message: 'No thought found with this id!'})
+              : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+};
